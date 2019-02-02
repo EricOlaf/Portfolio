1610a90fccfcee1bc13a653ab9b872c9585228a5
Eric Olaveson <olavesoneric@gmail.com>
2019-02-01 18:21:01 -0600

    more done on home page

--- src/component/Home/Home.js
+++ src/component/Home/Home.js
@@ -101,23 +101,22 @@ class Home extends Component {
               src="https://user-images.githubusercontent.com/41210242/47841519-dba2c080-dd87-11e8-9eaa-956e9b776a3e.png"
               alt=""
             />
-            <div className="siteExp">explanation</div>
+            <div className="siteExp">
+              Appy is Social Media site designed to uplift and encourage users.
+            </div>
           </div>
           <div className="site">
-            <div className="siteExp">explanation</div>
+            <div className="siteExp">
+              <div>
+                RawTrails, a site designed for users to find and scheule perfect
+                outdoor trip.
+              </div>
+            </div>
             <img
               className="sitePic"
               src="https://res.cloudinary.com/rawtrails801/image/upload/v1548807359/Screen_Shot_2019-01-29_at_6.09.41_PM.png"
               alt=""
             />
-          </div>
-          <div className="site">
-            <img
-              className="sitePic"
-              src="https://user-images.githubusercontent.com/41210242/47841519-dba2c080-dd87-11e8-9eaa-956e9b776a3e.png"
-              alt=""
-            />
-            <div className="siteExp">explanation</div>
           </div>
         </div>
       </div>
